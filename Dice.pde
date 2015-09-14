62f8faf51edcf55f1ebbbbc3c9f6faada47a2270
xmichellex <michellexi123@gmail.com>
2015-09-14 11:42:16 -0700

    changed index.html and the pde

--- Dice.pde
+++ Dice.pde
@@ -1,17 +1,22 @@
-Die tom;
 void setup()
 {
-	size(600, 600); 
+	size(570, 570); 
 	background(256, 256, 256);
-	tom = new Die(30, 40); 
 	noLoop();
 }
 void draw()
 {   
+
 	background(256, 256, 256);
-	tom.show(); 
-	tom.roll();
-	
+	for(int y = 50; y <= 450; y+=50) { 
+		for(int x = 50; x <= 450; x+=50) { 
+			Die tom = new Die(x, y);	
+			tom.show();	
+
+		}		
+
+
+	}
 
 
 }
@@ -29,8 +34,8 @@ class Die //models one single dice cube
 	}
 	void roll()
 	{
-		int dots = (int)(Math.random()) + 4;
-		System.out.println(dots);
+		int dots = (int)(Math.random()*6)+1;
+		
 		if(dots == 1)
 	 { 
 	 	fill(0); 
@@ -52,7 +57,26 @@ class Die //models one single dice cube
 	 	ellipse(myX+15, myY +35, 10, 10);
 	 	ellipse(myX+35, myY+17, 10, 10); 
 	 	ellipse(myX+35, myY+35, 10, 10);
+	 } else if(dots ==5) { 
+	 	fill(0); 
+	 	ellipse(myX+25, myY+25, 10, 10); 
+	 	ellipse(myX+15, myY+17, 10, 10); 
+	 	ellipse(myX+15, myY +35, 10, 10);
+	 	ellipse(myX+35, myY+17, 10, 10); 
+	 	ellipse(myX+35, myY+35, 10, 10);
+	 } else if(dots == 6) { 
+	 	fill(0); 
+	 	ellipse(myX+15, myY+13, 10, 10); 
+	 	ellipse(myX+15, myY+25, 10, 10); 
+	 	ellipse(myX+15, myY+37, 10, 10); 
+	 	ellipse(myX+35, myY+13, 10, 10); 
+	 	ellipse(myX+35, myY+25, 10, 10); 
+	 	ellipse(myX+35, myY+37, 10, 10); 
+
 	 }
+	
+	
+		System.out.println(dots);
 
 	 
 
@@ -60,8 +84,11 @@ class Die //models one single dice cube
 	void show()
 	{
 		//outline of the square 
-		fill(255, 0, 0); 
-		rect(myX, myY, 50, 50, 30); 
+		fill(255, 133, 247);
+		stroke(66, 14, 63);
+		strokeWeight(5);
+		rect(myX, myY, 50, 50, 50);
+		strokeWeight(2);
 		roll();
 	
 
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -14,20 +14,25 @@ import java.io.IOException;
 
 public class Dice extends PApplet {
 
-Die tom;
 public void setup()
 {
-	size(600, 600); 
+	size(570, 570); 
 	background(256, 256, 256);
-	tom = new Die(30, 40); 
 	noLoop();
 }
 public void draw()
 {   
+
 	background(256, 256, 256);
-	tom.show(); 
-	tom.roll();
-	
+	for(int y = 50; y <= 450; y+=50) { 
+		for(int x = 50; x <= 450; x+=50) { 
+			Die tom = new Die(x, y);	
+			tom.show();	
+
+		}		
+
+
+	}
 
 
 }
@@ -45,8 +50,8 @@ class Die //models one single dice cube
 	}
 	public void roll()
 	{
-		int dots = (int)(Math.random()) + 4;
-		System.out.println(dots);
+		int dots = (int)(Math.random()*6)+1;
+		
 		if(dots == 1)
 	 { 
 	 	fill(0); 
@@ -68,7 +73,26 @@ class Die //models one single dice cube
 	 	ellipse(myX+15, myY +35, 10, 10);
 	 	ellipse(myX+35, myY+17, 10, 10); 
 	 	ellipse(myX+35, myY+35, 10, 10);
+	 } else if(dots ==5) { 
+	 	fill(0); 
+	 	ellipse(myX+25, myY+25, 10, 10); 
+	 	ellipse(myX+15, myY+17, 10, 10); 
+	 	ellipse(myX+15, myY +35, 10, 10);
+	 	ellipse(myX+35, myY+17, 10, 10); 
+	 	ellipse(myX+35, myY+35, 10, 10);
+	 } else if(dots == 6) { 
+	 	fill(0); 
+	 	ellipse(myX+15, myY+13, 10, 10); 
+	 	ellipse(myX+15, myY+25, 10, 10); 
+	 	ellipse(myX+15, myY+37, 10, 10); 
+	 	ellipse(myX+35, myY+13, 10, 10); 
+	 	ellipse(myX+35, myY+25, 10, 10); 
+	 	ellipse(myX+35, myY+37, 10, 10); 
+
 	 }
+	
+	
+		System.out.println(dots);
 
 	 
 
@@ -76,8 +100,11 @@ class Die //models one single dice cube
 	public void show()
 	{
 		//outline of the square 
-		fill(255, 0, 0); 
-		rect(myX, myY, 50, 50, 30); 
+		fill(255, 133, 247);
+		stroke(66, 14, 63);
+		strokeWeight(5);
+		rect(myX, myY, 50, 50, 50);
+		strokeWeight(2);
 		roll();
 	
 
